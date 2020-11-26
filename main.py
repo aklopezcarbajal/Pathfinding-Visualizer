5a729fd5aa57ff5791b729459e6a3b7b964b1cdb
karenlc <aklopezcarbajal@gmail.com>
2020-11-25 19:45:31 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Pathfinding visualizer
+"""
+import pygame
+
+pygame.init()
+
+#Window
+W, H = 800,600
+screen = pygame.display.set_mode((W,H))
+
+#TODO:
+    #node class
+    #create grid
+    #implement algorithms
+    #UI
+
+
+
+
