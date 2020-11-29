e6d0980b4b194f8e46c07780d7688c7e8ddb66d2
karenlc <aklopezcarbajal@gmail.com>
2020-11-29 16:49:38 -0500

    Added node class.
    Created game loop and a function to draw a grid.

--- main.py
+++ main.py
@@ -7,7 +7,28 @@ pygame.init()
 
 #Window
 W, H = 800,600
-screen = pygame.display.set_mode((W,H))
+win = pygame.display.set_mode((W,H))
+
+NODESIZE = 15
+DIST = 5
+ROWS, COLS = int(W/(NODESIZE+DIST)), int(H/(NODESIZE+DIST) )
+color = (100,155,100)
+
+def draw_grid():
+    
+    for i in range(1,ROWS):
+        for j in range(1,COLS):
+            posX = int( i*DIST + (i-1)*NODESIZE + NODESIZE/2)
+            posY = int( j*DIST + (j-1)*NODESIZE + NODESIZE/2 )
+            pygame.draw.rect(win, color, (posX,posY,NODESIZE,NODESIZE) )
+ 
+running = True
+while running:  
+    win.fill((100,100,100))
+    draw_grid()
+    
+    pygame.display.update()
+            
 
 #TODO:
     #node class
---
+++ node.py
@@ -0,0 +1,29 @@
+"""
+Node class | Pathfinding Visualizer
+"""
+import numpy as np
+
+class node:
+    def __init__(self, row, col):
+        self.i = row
+        self.j = col
+        self.neighbors = []
+        self.visited = False
+        self.inQueue = False
+        
+    def find_neighbors(self, grid):
+        rows = np.size(grid, 0)
+        cols = np.size(grid, 1)
+        if self.i > 0:
+            self.neighbors.append(grid[self.i-1,self.j])
+        if self.i < rows-1:
+            self.neighbors.append(grid[self.i+1,self.j])
+        if self.j > 0:
+            self.neighbors.append(grid[self.i,self.j-1])
+        if self.j < cols-1:
+            self.neighbors.append(grid[self.i,self.j+1])
+            
+        
+        
+        
+    
